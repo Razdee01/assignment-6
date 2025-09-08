e1dee47bad37856b4dba2c08fc83b07b7f1a64ad
Razdee01 <rahmanrajdee@gmail.com>
2025-09-08 21:54:14 +0600

    full-functionality-done

--- js/index.js
+++ js/index.js
@@ -3,54 +3,48 @@ const loadCategories = () => {
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories));
 };
-const removeActive=()=>{
-  const treeCat=document.querySelectorAll(".tree-cat")
-    for(let cats of treeCat){
-      cats.classList.remove("active")
-    }
-  
-}
+const removeActive = () => {
+  const treeCat = document.querySelectorAll(".tree-cat");
+  for (let cats of treeCat) {
+    cats.classList.remove("active");
+  }
+};
 // ------Loading-----
-const manageSpiner=(status)=>{
-  if(status==true){
-    document.getElementById("spinner").classList.remove('hidden')
-    document.getElementById("all-cards").classList.add('hidden')
-  }else{
+const manageSpiner = (status) => {
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("all-cards").classList.add("hidden");
+  } else {
     document.getElementById("all-cards").classList.remove("hidden");
     document.getElementById("spinner").classList.add("hidden");
   }
-}
+};
 
 // onclick
-const loadTreeCategorie=(id)=>{
+const loadTreeCategorie = (id) => {
   manageSpiner(true);
-  const url=`https://openapi.programming-hero.com/api/category/${id}`
+  const url = `https://openapi.programming-hero.com/api/category/${id}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      removeActive()
+      removeActive();
       const clickCat = document.getElementById(`cat-${id}`);
-      clickCat.classList.add("active")
+      clickCat.classList.add("active");
       displayLoadTreeCategorie(data.plants);
-     
     });
-  
-  
-}
-    // Modal
-    const loadCatDetail=(id)=>{
-    
-      const url = `https://openapi.programming-hero.com/api/plant/${id}`;
-      console.log(url);
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => displayCatDetails(data.plants));
-       
-    }
-    
-    const displayCatDetails=(details)=>{
-      const detailsBox = document.getElementById("details-container");
-      detailsBox.innerHTML = `
+};
+// Modal
+const loadCatDetail = (id) => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCatDetails(data.plants));
+};
+
+const displayCatDetails = (details) => {
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
       <div class="">
       <h3 onclick='loadCatDetail(${details.id})'  class="text-xl font-semibold p-3 ">${details.name}</h3>
       <div class="mb-2 p-4">
@@ -68,17 +62,15 @@ const loadTreeCategorie=(id)=>{
    
   </div>
       `;
-      document.getElementById("my_modal_5").showModal();
-    
-    }
+  document.getElementById("my_modal_5").showModal();
+};
 
-const displayLoadTreeCategorie=(TreeCategories)=>{
-  
-  const treeCat=document.getElementById("all-cards")
-    treeCat.innerHTML=""
-    for(let TreeCategorie of TreeCategories){
-      const treeCatDiv=document.createElement("div")
-      treeCatDiv.innerHTML = `  
+const displayLoadTreeCategorie = (TreeCategories) => {
+  const treeCat = document.getElementById("all-cards");
+  treeCat.innerHTML = "";
+  for (let TreeCategorie of TreeCategories) {
+    const treeCatDiv = document.createElement("div");
+    treeCatDiv.innerHTML = `  
        <div class="p-4 shadow-lg bg-white h-full">
         <div class="mb-2 p-4">
           <img class=" md:h-48 w-full rounded-md" src=${TreeCategorie.image} alt="">
@@ -95,10 +87,10 @@ const displayLoadTreeCategorie=(TreeCategories)=>{
           <button class="btn bg-[#FACC15] text-[#15803D] rounded-full  w-full add-to-cart">Add To Card</button>
         </div>
        </div>`;
-  treeCat.append(treeCatDiv)
-    }
-    manageSpiner(false);
-}
+    treeCat.append(treeCatDiv);
+  }
+  manageSpiner(false);
+};
 
 displayCategories = (categories) => {
   const tressContainer = document.getElementById("catagories-list");
@@ -113,17 +105,16 @@ displayCategories = (categories) => {
     tressContainer.append(catlist);
   }
 };
-const allCategories=()=>{
-  manageSpiner(true)
+const allCategories = () => {
+  manageSpiner(true);
   fetch("https://openapi.programming-hero.com/api/plants")
-  .then(res=>res.json())
-  .then(data=>displayAllCat(data.plants))
-  
-}
-const displayAllCat=(everyCats)=>{
-  const allCat=document.getElementById("all-cards")
-  allCat.innerHTML=""
-  for(let everyCat of everyCats){
+    .then((res) => res.json())
+    .then((data) => displayAllCat(data.plants));
+};
+const displayAllCat = (everyCats) => {
+  const allCat = document.getElementById("all-cards");
+  allCat.innerHTML = "";
+  for (let everyCat of everyCats) {
     const allCatdivs = document.createElement("div");
     allCatdivs.innerHTML = `
     <div class="p-4 shadow-lg bg-white h-full">
@@ -146,17 +137,16 @@ const displayAllCat=(everyCats)=>{
     allCat.append(allCatdivs);
   }
   manageSpiner(false);
-}
+};
 // -------------Add to Cart-------------//
-cartTotal=0
-document.getElementById("all-cards").addEventListener("click", function(e){
+cartTotal = 0;
+document.getElementById("all-cards").addEventListener("click", function (e) {
   if (e.target.className.includes("add-to-cart")) {
     const addToCart = e.target;
     const heading =
       addToCart.parentNode.parentNode.children[1].children[0].innerText;
     const price =
       addToCart.parentNode.parentNode.children[2].children[2].innerText;
-
 
     alert(`${heading} has been added to the cart`);
     const cartContainer = document.getElementById("cart-container");
@@ -166,29 +156,36 @@ document.getElementById("all-cards").addEventListener("click", function(e){
     <div class="flex justify-between items-center bg-white w-full rounded-md  p-4">
       <div>
           <h3 class="font-medium text-xl">${heading}</h3>
-          <p class="text-sm">Price :${price}</p>
+          <p class="text-sm">Price :$<span class="item-price">${price}</span></p>
       </div>
       <!-- cross button -->
       <div>
-          <button class="btn bg-white border-none">❌</button>
+          <button class="btn bg-white border-none cross-btn">❌</button>
       </div>
     </div>
     `;
+
     cartContainer.append(cartDiv);
     // ----Total----//
     cartTotal = cartTotal + Number(price);
     document.getElementById("cart-total").innerText = `Total: $${cartTotal}`;
   }
 });
+// ---Removal Functionality---//
 
+  document.getElementById("cart-container").addEventListener("click", function (e){
+    if (e.target.className.includes("cross-btn")) {
+      const cross = e.target.parentNode.parentNode; 
+      cross.remove(); 
+      const price =cross.querySelector(".item-price").innerText
+      cartTotal = cartTotal - Number(price);
+      document.getElementById("cart-total").innerText = `Total: $${cartTotal}`;
+      
+    }
+  })
 
-
-
-
-
-
-
-
-
-allCategories()
+ 
+  
+    
+allCategories();
 loadCategories();
